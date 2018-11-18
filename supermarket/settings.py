38ee1c7e045d714a5b9d616758425e6f11194c9a
mylove <332960063@qq.com>
2018-11-18 16:48:48 +0800

    xxx1

--- supermarket/settings.py
+++ supermarket/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'supermarket.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), #数据库修改
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': os.path.join(BASE_DIR, "my.cnf"),  # 从文件中读取连接信息
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), #数据库修改
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'OPTIONS': {
+        #     'read_default_file': os.path.join(BASE_DIR, "my.cnf"),  # 从文件中读取连接信息
+        # },
     }
 }
 
